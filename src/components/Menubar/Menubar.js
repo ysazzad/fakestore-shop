3df5f044ac2d3eeb51c6cf15e6c2b0f99cbda57a
sazzad <hossensazzad1201@gmail.com>
2022-08-10 16:43:17 +0600

    navbar added

---
+++ src/components/Menubar/Menubar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Menubar.css'
+
+const Menubar = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2 p-2"> logo</div>
+                    <div className="col-md-10 menu-container d-flex justify-content-end">
+                        <li className='items me-4 p-2' >Home</li>
+                        <li className='items me-4 p-2'>Contact</li>
+                        <li className='items me-4 p-2'>Cart</li>
+                        <li className='items me-4 p-2'>Login</li>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Menubar;

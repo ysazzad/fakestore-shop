3bc1cf8a483e533cade25a5c2ff7e710cefcd77c
sazzad <hossensazzad1201@gmail.com>
2022-08-11 18:00:49 +0600

    aos animation added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menubar from './components/Menubar/Menubar';
 import AllProducts from './components/AllProducts/AllProducts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   const [count, setCount] = useState(0)
   const setCartCount = () => {
     setCount(count + 1)
   }
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="App">
       <Menubar count={count}></Menubar>
--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -3,7 +3,8 @@ import ReactModal from '../ReactModal/ReactModal';
 
 const SingleProduct = ({ product, setCartCount }) => {
     return (
-        <div className="col-md-4 ">
+        <div data-aos="flip-left" data-aos-easing="linear"
+            data-aos-duration="2000" className="col-md-4 ">
             <div className="cart p-3 border h-80">
                 <img src={product.image} className="w-50 m-auto" alt="" />
                 <h4>{product.title.slice(0, 30)}</h4>
